3b2ce16388f82ce69f56ebe65660ce4154a6c61d
sidharth <sidharthmalhotra0094@gmail.com>
2018-08-18 12:17:06 -0400

    [WIP] Added route for IdeaDetail Component

--- src/app/routes/opinionize.routes.ts
+++ src/app/routes/opinionize.routes.ts
@@ -1,8 +1,18 @@
+import { IdeaDetailComponent } from './../modules/dashboard/idea-detail/idea-detail.component';
 import { DashboardComponent } from './../modules/dashboard/dashboard/dashboard.component';
 import { LandingComponent } from './../modules/front/landing/landing.component';
 import { Routes } from '@angular/router';
 
-export const opRoutes: Routes =[
- {path: '', component: LandingComponent},
- {path: 'dashboard', component: DashboardComponent}
+export const opRoutes: Routes = [
+  { path: '', component: LandingComponent },
+  {path: 'dashboard', component: DashboardComponent},
+  {
+    path: 'idea',
+    children: [
+      {
+        path: ':id',
+        component: IdeaDetailComponent
+      }
+    ]
+  }
 ]
