ccd80b515af9be6fdd40b093d6b2d9ad19ba112e
sidharth <sidharthmalhotra0094@gmail.com>
2018-08-17 22:15:09 -0400

    [WIP] Added toolbar to dashboard component and initialized display-posts component

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,13 +3,16 @@ import { MaterialModule } from './../material/material.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule } from '../../../../node_modules/@angular/router';
+import { DisplayPostsComponent } from './display-posts/display-posts.component';
 
 @NgModule({
   imports: [
     CommonModule,
     MaterialModule,
+    RouterModule,
     FlexLayoutModule
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent, DisplayPostsComponent]
 })
 export class DashboardModule { }
--- src/app/routes/opinionize.routes.ts
+++ src/app/routes/opinionize.routes.ts
@@ -1,6 +1,8 @@
+import { DashboardComponent } from './../modules/dashboard/dashboard/dashboard.component';
 import { LandingComponent } from './../modules/front/landing/landing.component';
 import { Routes } from '@angular/router';
 
 export const opRoutes: Routes =[
- {path: '', component: LandingComponent}
+ {path: '', component: LandingComponent},
+ {path: 'dashboard', component: DashboardComponent}
 ]
